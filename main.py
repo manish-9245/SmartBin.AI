75b1d164013561358f21c98016253268442b3421
AdiKulkarni21 <1si19ei001@sit.ac.in>
2022-05-21 18:42:52 +0530

    db integration

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "gfhfghfghfg"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(300), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"Text:{self.id}"
 
 @app.route('/')
 def index():
